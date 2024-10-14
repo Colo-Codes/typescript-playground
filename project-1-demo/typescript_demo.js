0155b5c82f9a69e1e01d32e9cfcbda474bb26bf4
Damian Demasi <damian.demasi@aligent.com.au>
2024-10-15 09:56:08 +1030

    TypeScript basic types - Pizza app

---
+++ project-1-demo/typescript_demo.js
@@ -0,0 +1,7 @@
+// This is an industrial-grade general-purpose greeter function:
+function greet(person, date) {
+    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
+}
+greet("Brendan", new Date());
+var message = 'some text';
+var myName = null;
---
+++ project-2-typescript-basics-javascript/index.js
@@ -0,0 +1,43 @@
+"use strict";
+let ORDER_NUMBER = 0;
+const menu = [
+    { name: "Margherita", price: 8.99 },
+    { name: "Peperoni", price: 10.99 },
+    { name: "Hawaiian", price: 12.99 },
+    { name: "Veggie", price: 11.99 },
+];
+let cashInRegister = 100;
+const orderQueue = [];
+function addNewPizza(pizzaObject) {
+    menu.push(pizzaObject);
+}
+function placeOrder(pizzaName) {
+    const pizzaObject = menu.find((pizza) => pizza.name === pizzaName);
+    if (!pizzaObject)
+        return;
+    cashInRegister += pizzaObject.price;
+    ORDER_NUMBER += 1;
+    const orderObject = {
+        id: ORDER_NUMBER,
+        pizza: pizzaObject,
+        status: "ordered",
+    };
+    orderQueue.push(orderObject);
+    return orderObject;
+}
+function completeOrder(orderId) {
+    const orderToComplete = orderQueue.find((order) => order.id === orderId);
+    if (!orderToComplete)
+        return;
+    orderToComplete.status = "completed";
+    return orderToComplete;
+}
+// ////// OUTPUTS ////// //
+addNewPizza({ name: "Sauron's Fiery Feast", price: 15.0 });
+addNewPizza({ name: "Elven Garden Delight", price: 9.99 });
+addNewPizza({ name: "Gondor's White Tree Supreme", price: 13.99 });
+console.log(">>> Menu:", menu);
+console.log(">>> Cash in register:", cashInRegister);
+console.log(">>> Order placed:", placeOrder("Sauron's Fiery Feast"));
+console.log(">>> Order completed:", completeOrder(1));
+console.log(">>> Orders in Queue", orderQueue);
